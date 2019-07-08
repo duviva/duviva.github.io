1b255c2b11538197e06568ea36570fb57b9b43c0
duviva <52681106+duviva@users.noreply.github.com>
2019-07-09 02:52:10 +0300

    calendar 09.07.2019

---
+++ js/records.js
@@ -0,0 +1,7 @@
+var events = [
+  {'Date': new Date(2019, 06, 13), 'Title': '<b>Напиться!</b> <br> Витя Костин, Петр Михайлов.'},
+  {'Date': new Date(2019, 06, 18), 'Title': 'ДР! <br> Дима Молодцов!'},
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
