949a294f3555a82652470b0311bb88531c93c903
duviva <52681106+duviva@users.noreply.github.com>
2019-07-15 19:16:21 +0300

    Delete records.js

--- js/records.js
+++
@@ -1,7 +0,0 @@
-var events = [
-  {'Date': new Date(2019, 06, 13), 'Title': '<b>Напиться!</b> <br> Витя Костин, Петр Михайлов.'},
-  {'Date': new Date(2019, 06, 18), 'Title': 'ДР! <br> Дима Молодцов!'},
-];
-var settings = {};
-var element = document.getElementById('caleandar');
-caleandar(element, events, settings);
